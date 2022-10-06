2f2dcb6f9777ef7a662747ad55273d0724580ae5
Carlos Toledo <carlombo@hotmail.com>
2022-10-06 02:07:57 -0300

    Se arregla problema con las busquedas en mayúscula

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,7 +2,7 @@ class News < ApplicationRecord
   def self.buscar(text)
       if text and !text.empty? 
         found_notic = News.where( 
-          "encabezado LIKE ? or encabezado LIKE ? or encabezado LIKE ?", "#{text.capitalize}%", "%#{text.downcase}%", "%#{text.first.upcase}%"
+          "encabezado LIKE ? or encabezado LIKE ?", "%#{text.capitalize}%", "%#{text.downcase}%" 
         )
         if found_notic
           found_notic
