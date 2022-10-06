a6e8a0cd3dad081918ba4f96806d5d4960da126e
Carlos Toledo <carlombo@hotmail.com>
2022-10-06 01:42:30 -0300

    Se arregla la busqueda con la primera en mayúscula

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,7 +2,7 @@ class News < ApplicationRecord
   def self.buscar(text)
       if text and !text.empty? 
         found_notic = News.where( 
-          "encabezado LIKE ? or encabezado LIKE ?", "#{text.capitalize}%", "%#{text.downcase}%"
+          "encabezado LIKE ? or encabezado LIKE ? or encabezado LIKE ?", "#{text.capitalize}%", "%#{text.downcase}%", "%#{text.first.upcase}%"
         )
         if found_notic
           found_notic
